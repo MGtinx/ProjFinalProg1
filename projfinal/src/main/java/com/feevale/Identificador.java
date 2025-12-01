870c2677683410fff24314654830c2bf6c1c53e6
MGtinx <160495312+MGtinx@users.noreply.github.com>
2025-12-01 10:27:07 -0300

    Deletar projfinal directory

--- projfinal/src/main/java/com/feevale/Identificador.java
+++
@@ -1,5 +0,0 @@
-package com.feevale;
-public interface Identificador {
-    
-    public abstract int geradorId();
-}
--- projfinal/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.feevale {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.feevale to javafx.fxml;
-    exports com.feevale;
-}
